1f0f5f93b375c6ecea35f047a529e18d6366b70b
akzshah-code <akz.shah@gmail.com>
2025-09-24 16:56:20 +0530

    proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure this is set to the root path
-});
+  server: {
+    proxy: {
+      // Proxy /api requests to your Flask backend
+      '/api': {
+        target: 'http://127.0.0.1:5000', // Your Flask server address
+        changeOrigin: true,
+      },
+    }
+  }
+})
